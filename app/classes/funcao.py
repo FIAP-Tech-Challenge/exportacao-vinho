99f85528ffc9fcff3d5ae66b2043566b7dbb7747
RafaelInoue <RafaelInoue@PAYHOP-DEV-01>
2024-01-21 11:15:57 -0300

    fix: função importacao

--- app/classes/funcao.py
+++ app/classes/funcao.py
@@ -123,7 +123,7 @@ def graph_1(df: pl.DataFrame, config: dict) -> None:
     
     layout_info_selected = layout_info[config['metric']][config['agg']]
 
-    f.layout_graphs(
+    layout_graphs(
         fig,
         yaxis=layout_info_selected["yaxis"],
         xaxis={"title": "Região"},
@@ -198,7 +198,7 @@ def graph(df: pl.DataFrame, config:dict) -> None:
         
         layout_info_selected = layout_info[config['metric']]
 
-        f.layout_graphs(
+        layout_graphs(
             fig,
             yaxis=layout_info_selected["yaxis"],
             xaxis={"title": "Ano de Referência"},
@@ -217,3 +217,58 @@ def graph(df: pl.DataFrame, config:dict) -> None:
             title_sup=layout_info_selected["sup"]
         )     
     return fig
+
+def layout_graphs(
+    fig: px,
+    template: str = "seaborn",
+    height: int = 500,
+    margin: dict = {"l": 10, "r": 10, "b": 10, "t": 85, "pad": 5},
+    legend: dict = {
+        "orientation": "v",
+        "yanchor": "middle",
+        "xanchor": "center",
+        "x": 1,
+        "y": 0.5,
+        "title": "",
+        "itemsizing": "constant",
+    },
+    xaxis: dict = {"title": ""},
+    yaxis: dict = {"title": ""},
+    hovertemplate: str = "<b>%{x}</b><br>%{y}",
+    title_text = "-",
+    title_sup = "--",
+    marker_color = '',
+    line_color = '',
+    other: dict = dict(),
+) -> None:
+    dic = dict(
+        template=template,
+        height=height,
+        margin=margin,
+        legend=legend,
+        xaxis=xaxis,
+        yaxis=yaxis,
+        title={
+            'text': f'{title_text}<br><sup style="color: #888; font-weight: normal;">{title_sup}</sup>',
+            'xanchor': 'left',
+            'xref': 'paper',
+            'yanchor': 'auto',
+            'x': 0,
+            'y': .95,
+            'font': {
+                'size': 20,
+                'color': '#666'
+            }
+        },
+    )
+
+    fig.update_layout(dic | other)
+    
+    if marker_color != '':
+        fig.update_traces(marker_color = marker_color)
+    if line_color != '':
+        fig.update_traces(line_color = line_color)
+    
+    fig.update_traces(hovertemplate=hovertemplate)
+
+    return None
--- app/pages/4_🌏_Importacao.py
+++ app/pages/4_🌏_Importacao.py
@@ -1,23 +1,8 @@
+import requests
 import streamlit as st
 import polars as pl
 import plotly.express as px
-import requests
-from . import funcao as f0
-
-url_imp = 'https://raw.githubusercontent.com/FIAP-Tech-Challenge/exportacao-vinho/main/app/data/importacao.csv'
-response_imp = requests.get(url_imp)     
-if response_imp.status_code == 200:
-    # Assuming it's a text file
-    content = response_imp.text
-
-    # Now, you can work with the content as needed
-    with open('file_imp.csv', 'w') as local_file_imp:
-        local_file_imp.write(content)
-else:
-    print(f"Failed to download file. Status code: {response_imp.status_code}")   
-
-with open(local_file_imp.name) as file:
-    df = pl.read_csv(local_file_imp,truncate_ragged_lines=True)
+from classes import funcao as f
 
 st.markdown("<p style='text-align: center; color:purple;font-size:54px'> Importação de Vinhedos 🌏</p>",  unsafe_allow_html=True)
 
@@ -29,18 +14,32 @@ st.markdown("<p style='text-align: justify; color:gray; font-size:18px'>Entre 20
 
 st.markdown("""**Fonte de Dados:** [Banco de dados de uva, vinho e derivados](http://vitibrasil.cnpuv.embrapa.br/)""")
 
+with st.expander("↓ Download dos Arquivos", expanded=False):
+    url_imp = 'https://raw.githubusercontent.com/FIAP-Tech-Challenge/exportacao-vinho/main/app/data/importacao.csv'
+    response_imp = requests.get(url_imp)     
+    if response_imp.status_code == 200:
+        # Assuming it's a text file
+        content = response_imp.text
 
-with open(local_file_imp.name) as file:
-    btn = st.download_button(
-        label="📊 Baixar CSV importação vinho",
-        data=file,
-        file_name="importacao.csv",
-        mime='text/csv',
-        type='primary',
-        key="download_csv_imp"
-    )
+        # Now, you can work with the content as needed
+        with open('file_imp.csv', 'w') as local_file_imp:
+            local_file_imp.write(content)
+    else:
+        print(f"Failed to download file. Status code: {response_imp.status_code}")   
+
+    with open(local_file_imp.name) as file:
+        btn = st.download_button(
+            label="📊 Baixar CSV importação vinho",
+            data=file,
+            file_name="importacao.csv",
+            mime='text/csv',
+            type='primary',
+            key="download_csv_imp"
+        )
 
 st.markdown("<div style='margin-bottom: 40px'></div>", unsafe_allow_html=True)
+
+df = pl.read_csv("https://raw.githubusercontent.com/FIAP-Tech-Challenge/exportacao-vinho/main/app/data/importacao.csv",truncate_ragged_lines=True)
 
 f.tab_intro(df)
 
